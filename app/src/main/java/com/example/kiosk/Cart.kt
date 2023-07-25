6e70789e0db64378ef1995b97a1ed6a63df14761
ars-yeon <zosel7301@gmail.com>
2023-07-25 15:33:29 +0900

    [FEAT]: ⚙️ 2023.07.25.PM3:32 메뉴와 수량 입력시 카트에 저장하는 기능 구현

---
+++ app/src/main/java/com/example/kiosk/Cart.kt
@@ -0,0 +1,51 @@
+package com.example.kiosk
+
+data class CartItem(val name: String, val price: Double, val quantity: Int)  // 메뉴명, 가격, 수량
+
+class Cart {
+    private val items = mutableListOf<CartItem>()
+
+    /*fun addToCart(item: TangMenu, quantity: Int) {
+        items.add(CartItem(item.name, item.price, quantity))
+    }*/
+
+    fun addToCart(item: DryMenu, quantity: Int) {
+        items.add(CartItem(item.name, item.price, quantity))
+    }
+
+    /*fun addToCart(item: RiceMenu, quantity: Int) {
+        items.add(CartItem(item.name, item.price, quantity))
+    }
+
+    fun addToCart(item: DrinkMenu, quantity: Int) {
+        items.add(CartItem(item.name, item.price, quantity))
+    }*/
+
+    fun displayCart() {
+        if (items.isEmpty()) {
+            println("---카트가 비어 있습니다.---")
+            return
+        } else {
+            println("===Cart===")
+            var totalCost = 0.0
+            for ((index, item) in items.withIndex()) {
+                val subtotal = item.price * item.quantity  // 메뉴별로 가격 계산(여러개 선택할 수 있기 때문)
+                println("${index+1}. ${item.name} | ${item.quantity}개")
+                totalCost += subtotal  // 총액에 메뉴별 가격 더하기
+            }
+            println("Total: $totalCost")
+        }
+
+        while (true) {
+            print("0. 뒤로가기\n- 입력: ")
+            val input = readLine()!!
+
+            if (input=="0") {
+                return
+            } else {
+                println("※ 잘못된 입력입니다.")
+            }
+        }
+    }
+
+}
