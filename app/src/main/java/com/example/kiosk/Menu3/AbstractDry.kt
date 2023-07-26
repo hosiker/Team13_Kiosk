2d794ba1304efde30cbf5c43f7c417467cfbf5d1
ars-yeon <zosel7301@gmail.com>
2023-07-26 12:32:27 +0900

    [FEAT]: ⚙️ 2023.07.26.PM12:32 - AbstractDry: 추상클래스 내 추상메서드인 abstractDry의 매개변수 삭제

--- app/src/main/java/com/example/kiosk/Menu3/AbstractDry.kt
+++ app/src/main/java/com/example/kiosk/Menu3/AbstractDry.kt
@@ -1,8 +1,9 @@
 package com.example.kiosk.Menu3
 
+import com.example.kiosk.Utils.MenuItem
+
 // 마른안주 추상화
 abstract class AbstractDry {
-    abstract fun abstractDry(name: String, price: Int, depict: String): MenuItem
+    abstract fun abstractDry(): MenuItem
 }
 
-data class MenuItem(val name: String, val price: Int, val depict: String)
