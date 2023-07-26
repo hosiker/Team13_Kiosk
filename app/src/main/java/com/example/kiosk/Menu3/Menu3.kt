582dae8ddc1574d5c4d0b62caaf84ab257f72c38
ars-yeon <zosel7301@gmail.com>
2023-07-26 12:36:25 +0900

    [FEAT]: ⚙️ 2023.07.26.PM12:32 - val quantity를 QuantityUtil을 불러와서 실행하도록 수정 - val pick을 val inputQuantity로 수정 -

--- app/src/main/java/com/example/kiosk/Menu3/Menu3.kt
+++ app/src/main/java/com/example/kiosk/Menu3/Menu3.kt
@@ -1,6 +1,8 @@
 package com.example.kiosk.Menu3
 
-import com.example.kiosk.Cart
+import com.example.kiosk.Utils.Cart
+import com.example.kiosk.Utils.MenuItems
+import com.example.kiosk.Utils.QuantityUtils
 
 open class Menu3() {
     fun dry() {
@@ -13,35 +15,30 @@ open class Menu3() {
                 println("${index+1}. ${menuItem.name} | W ${menuItem.price} | ${menuItem.depict}")
             }
             println("5. 카트보기")
+            println("6. 총 가격 보기")
             println("0. 뒤로가기")
 
             print("입력: ")
-            val pick = readLine()!!.toIntOrNull() //  입력값을 정수로 변환 가능하면 해당 정수값을 반환, 그렇지 않면 null 반환
+            val inputQuantity = readLine()?.toIntOrNull() //  입력값을 정수로 변환 가능하면 해당 정수값을 반환, 그렇지 않면 null 반환
 
-            if (pick != null) {
-                when (pick) {
+            if (inputQuantity != null) {
+                when (inputQuantity) {
                     in 1..menu3Items.size -> { // 1~menu3item 리스트 크기동안 실행
-                        val selectedItem = menu3Items[pick.toInt()-1]  // 선택한 메뉴의 인덱스에 해당하는 메뉴를 가져옴
-                        println("${pick}. ${selectedItem.name}을(를) 선택하셨습니다.")
+                        val selectedItem = menu3Items[inputQuantity.toInt()-1]  // 선택한 메뉴의 인덱스에 해당하는 메뉴를 가져옴
+                        println("${inputQuantity}. ${selectedItem.name}을(를) 선택하셨습니다.")
 
-                        println("수량을 입력해주세요.")
-                        val quantity = readLine()!!.toIntOrNull() //  입력값을 정수로 변환 가능하면 해당 정수값을 반환, 그렇지 않면 null 반환
-                        if (quantity != null && quantity > 0) {  // 입력값이 숫자이고 0 이상이라면
-                            cart.addToCart(selectedItem.name, selectedItem.price, quantity)  // 카트에 메뉴를 추가하는 함수를 호출해 - 메뉴명, 가격, 수량 정보를 인자로 전달해서 카트에 해당 메뉴를 추가
-                            println("${selectedItem.name} ${quantity}개가 카트에 담겼습니다.\n")
-                        } else {
-                            println("※ 올바른 수량을 입력해주세요.")
-                            continue  // null 값일 경우 다시 입력하게 설정
-                        }
+                        val quantity = QuantityUtils.askForQuantity()
+                        cart.addToCart(selectedItem.name, selectedItem.price, quantity)
+                        println("${selectedItem.name} ${quantity}개가 카트에 담겼습니다.")
                     }
 
                     5 -> {
                         cart.viewCart()  // 카드에 담긴 항목을 보여줌
+                        continue
                     }
 
                     6 -> {
-                        println(cart.total())  // 총합을 선택하면 가격을 말해주고 계산하는걸로 넘어가야 하는걸까?
-                        // cart.total()
+                        cart.total()  // 총합을 선택하면 가격을 말해주고 계산하는걸로 넘어가야 하는걸까?
                     }
 
                     0 -> {
