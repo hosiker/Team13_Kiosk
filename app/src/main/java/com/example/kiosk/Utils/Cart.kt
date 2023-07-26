6e9dd46b5416461365b7389fd54977132ab428f9
ars-yeon <zosel7301@gmail.com>
2023-07-26 12:27:52 +0900

    [FEAT]: ⚙️ 2023.07.26.PM12:27 - class Cart: viewCart()함수와 total()함수에 뒤로가기 기능이 중복되서 viewCart()함수에서의 내용 삭제

--- app/src/main/java/com/example/kiosk/Utils/Cart.kt
+++ app/src/main/java/com/example/kiosk/Utils/Cart.kt
@@ -1,5 +1,4 @@
-package com.example.kiosk
-
+package com.example.kiosk.Utils
 
 class Cart() {
     val cartItems: MutableList<Triple<String, Int, Int>> = mutableListOf()  // 메뉴명, 가격, 수량
@@ -13,11 +12,7 @@ class Cart() {
             cartItems.find { it.first == name && it.second == price }  // 카트아이템 리스트에 이름과 가격이 일치하는 항목을 검색하는 코드
 
         if (existingItem != null) { // 항목이 이미 존재한다면
-            val updatedItem = Triple(
-                existingItem.first,
-                existingItem.second,
-                existingItem.third + quantity
-            )  // 수량을 더해주고 updatedItem 저장
+            val updatedItem = Triple(existingItem.first, existingItem.second, existingItem.third + quantity)  // 수량을 더해주고 updatedItem 저장
             cartItems.remove(existingItem)  // 이미 존재하는 항목의 수량을 수정하기 위해 먼저 제거
             cartItems.add(updatedItem)  // 위에서 제거한 항목을 수량이 변한 변수로 추가
         } else { // 항목에 존재하지 않는다면 새롭게 추가
@@ -32,37 +27,44 @@ class Cart() {
         return cartItems.isEmpty()
     }
 
+
+    // 카트 목록 보여주는 함수
     fun viewCart() {
         if (isEmpty()) {
-            println("---카트가 비어 있습니다.---\n")
+            println("\n---카트가 비어 있습니다.---")
             return  // 비어 있을 때는 항목을 딱히 안봐도 된다고 생각해서 우선 카트에서 뒤로가기가 자동으로 되게 해둠
         } else {
             println("\n===카트 목록===")
             for ((index, cartItem) in cartItems.withIndex()) {
                 println("${index + 1}. ${cartItem.first} | W ${cartItem.second} | ${cartItem.third}개")
             }
-            println(total())
-        }
-
-        while (true) {
-            print("\n0. 뒤로가기\n입력: ")
-            val back = readLine()!!.toIntOrNull()
-            if (back == 0) { // 0을 입력하면 뒤로가기
-                return
-            } else {  // 0 외의 값을 입력하면 다시 입력하게 하기
-                println("※ 잘못된 입력입니다.")
-            }
+            total()  // total()함수에 출력 기능이 있으니까 중복되지 않게 그냥 함수만 불러오기
         }
     }
 
-    
-    // 총합을 구하는 합수
-    fun total(): String {  // return 값을 'Total  W $totalCost' 이런 형식으로 반환하기 위해 String으로 지정
+
+    // 카트 내 항목의 총합을 구하는 합수
+    fun total() {
         var totalCost = 0  // 초기값
         for (cartItem in cartItems) {  // 카트에 담긴 항목들을 하나씩 다 둘러보는거
             val subtotal = cartItem.second * cartItem.third  // 각 메뉴별 가격 계산
             totalCost += subtotal  // totalCost(총합)에 subtotal(각 메뉴별 계산)을 더함
         }
-        return "Total  W $totalCost"
+        println("Total  W $totalCost")  // 총합 출력
+        
+        while (true) {
+            if (totalCost == 0) {  // 총합이 0이면 자동으로 뒤로가기
+                return
+            } else {  // 0을 입력하면 뒤로가기, 다른 값을 입력하면 다시 입력하게 함
+                println("0. 뒤로가기")
+                val back = readLine()?.toIntOrNull()
+                if (back == 0) {
+                    return
+                } else {
+                    println("다시 입력해 주세요.")
+                    continue
+                }
+            }
+        }
     }
 }
